5aa27bc7cf23bee8f5098b18a4d7dd69d1e09135
mohammedAK1991 <mohmed.ak.17@gmail.com>
2022-08-24 19:36:20 +0200

    chore: install chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
