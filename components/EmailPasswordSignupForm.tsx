ccd8593c34adfc7d86aa9114e86653efe65f86d1
mohammedAK1991 <mohmed.ak.17@gmail.com>
2022-08-25 21:14:59 +0200

    feat(signup): basic signup page and EmailPasswordSignup component

---
+++ components/EmailPasswordSignupForm.tsx
@@ -0,0 +1,196 @@
+import React, { useCallback, useState } from 'react';
+import {
+  Button,
+  VStack,
+  chakra,
+  FormControl,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Icon,
+} from '@chakra-ui/react';
+import { EmailIcon, LockIcon } from '@chakra-ui/icons';
+import { FaEye } from 'react-icons/fa';
+import { IoIosPerson } from 'react-icons/io';
+
+export default function EmailPasswordSignUpForm({
+  handleEmailSignUp,
+}: {
+  handleEmailSignUp: (name: string, email: string, password: string) => void;
+}) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleNameInputChange = useCallback(
+    (ev: { currentTarget: { value: React.SetStateAction<string> } }) =>
+      setName(ev.currentTarget.value),
+    [],
+  );
+  const handleEmailInputChange = useCallback(
+    (ev: { currentTarget: { value: React.SetStateAction<string> } }) =>
+      setEmail(ev.currentTarget.value),
+    [],
+  );
+  const handlePasswordInputChange = useCallback(
+    (ev: { currentTarget: { value: React.SetStateAction<string> } }) =>
+      setPassword(ev.currentTarget.value),
+    [],
+  );
+  const handleConfirmPasswordInputChange = useCallback(
+    (ev: { currentTarget: { value: React.SetStateAction<string> } }) =>
+      setConfirmPassword(ev.currentTarget.value),
+    [],
+  );
+  const handleShowPasswordClick = useCallback(
+    () => setShowPassword((prevValue) => !prevValue),
+    [],
+  );
+
+  const handleEmailPasswordSignUpSubmit = useCallback(
+    (ev: { preventDefault: () => void }) => {
+      ev.preventDefault();
+      handleEmailSignUp(name, email, password);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [name, email, password],
+  );
+
+  return (
+    <chakra.form onSubmit={handleEmailPasswordSignUpSubmit}>
+      <VStack minW="360px" align="center" spacing="6">
+        <FormControl id="name" rounded="md">
+          <InputGroup>
+            <InputLeftElement mt="1" pointerEvents="none">
+              <Icon as={IoIosPerson} color="black" />
+            </InputLeftElement>
+            <Input
+              type="text"
+              value={name}
+              bgColor="white"
+              onChange={handleNameInputChange}
+              variant="outline"
+              placeholder={'name'}
+              size="lg"
+              isRequired
+              _focus={{
+                borderColor: 'secondary',
+              }}
+              _placeholder={{
+                color: 'onSurfaceVariant',
+              }}
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl id="email" rounded="md">
+          <InputGroup>
+            <InputLeftElement mt="1" pointerEvents="none">
+              <EmailIcon color="black" />
+            </InputLeftElement>
+
+            <Input
+              type="text"
+              value={email}
+              bgColor="white"
+              onChange={handleEmailInputChange}
+              variant="outline"
+              placeholder={'email'}
+              size="lg"
+              isRequired
+              _focus={{
+                borderColor: 'secondary',
+              }}
+              _placeholder={{
+                color: 'onSurfaceVariant',
+              }}
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl id="password" rounded="md">
+          <InputGroup mb="4">
+            <InputLeftElement mt="1" pointerEvents="none">
+              <LockIcon color="black" />
+            </InputLeftElement>
+            <Input
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={handlePasswordInputChange}
+              variant="outline"
+              bgColor="white"
+              placeholder={'password'}
+              size="lg"
+              isRequired
+              _focus={{
+                borderColor: 'secondary',
+              }}
+              _placeholder={{
+                color: 'onSurfaceVariant',
+              }}
+            />
+
+            <InputRightElement mt="1" cursor="pointer">
+              <Icon
+                as={FaEye}
+                color="black"
+                onClick={handleShowPasswordClick}
+              />
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+
+        <FormControl id="confirmPassword" rounded="md">
+          <InputGroup>
+            <InputLeftElement mt="1" pointerEvents="none">
+              <LockIcon color="black" />
+            </InputLeftElement>
+            <Input
+              type={showPassword ? 'text' : 'password'}
+              value={confirmPassword}
+              onChange={handleConfirmPasswordInputChange}
+              variant="outline"
+              bgColor="white"
+              placeholder={'repeat password'}
+              size="lg"
+              py={5}
+              px={4}
+              isRequired
+              _placeholder={{
+                color: 'onSurfaceVariant',
+              }}
+            />
+            <InputRightElement cursor="pointer">
+              <Icon
+                as={FaEye}
+                color="black"
+                onClick={handleShowPasswordClick}
+              />
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+
+        <Button
+          disabled={
+            !email ||
+            !name ||
+            !password ||
+            !confirmPassword ||
+            password !== confirmPassword
+          }
+          alignSelf="stretch"
+          size="lg"
+          type="submit"
+          bgColor="#f4a261"
+          onClick={handleEmailPasswordSignUpSubmit}
+          color="black"
+        >
+          sign up
+        </Button>
+      </VStack>
+    </chakra.form>
+  );
+}
---
+++ pages/signup.tsx
@@ -0,0 +1,131 @@
+import React, { FormEvent, useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+import firebase from '../data/firebase';
+import {
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  Stack,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { FaGoogle } from 'react-icons/fa';
+import useAuth from '../data/useAuth';
+import EmailPasswordSignUpForm from '../components/EmailPasswordSignupForm';
+
+export default function Signup() {
+  const router = useRouter();
+  const { auth, signUpWithEmail, signUpWithGoogle } = useAuth();
+
+  const handleSignIn = useCallback(
+    async (credential: firebase.auth.UserCredential) => {
+      try {
+        if (!credential.user) return;
+        console.log(`Welcome ${credential.user.email}`);
+
+        // Check for the creation time to see if it's a new user.
+        if (
+          credential.user.metadata.creationTime ===
+          credential.user.metadata.lastSignInTime
+        ) {
+          // TODO : function that creates a user documents in users collection in firestore
+        }
+
+        router.push('/');
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [router],
+  );
+
+  const handleSignupWithGoogle = useCallback(() => {
+    signUpWithGoogle()
+      .then((credential) => handleSignIn(credential))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleEmailSignUp = useCallback(
+    (name: string, email: string, password: string) => {
+      signUpWithEmail(email, password)
+        .then((firebaseCredential) => {
+          firebaseCredential.user?.updateProfile({ displayName: name });
+          return firebaseCredential;
+        })
+        .then((firebaseCredential) => handleSignIn(firebaseCredential))
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    [signUpWithEmail],
+  );
+
+  return (
+    <Flex
+      bg="surface"
+      flexGrow={1}
+      direction="column"
+      h="100vh"
+      w="100vw"
+      bgColor="#2a9d8f"
+    >
+      <Stack align="center" rounded="md" bg="surface" m="auto" spacing="6">
+        <Flex align="center" alignSelf="stretch" direction="row">
+          <Heading mx="auto" fontWeight="700" fontSize="3xl">
+            Signup
+          </Heading>
+        </Flex>
+
+        <Flex direction="row">
+          <Text fontSize="sm">Already a user ?</Text>
+
+          <Link href="/login" passHref>
+            <Text
+              fontSize="sm"
+              fontWeight="600"
+              cursor="pointer"
+              textDecoration="underline"
+              color="blueAccent"
+              ml="2"
+            >
+              Login
+            </Text>
+          </Link>
+        </Flex>
+
+        <Stack align="center" spacing="6">
+          <Button
+            size="lg"
+            onClick={handleSignupWithGoogle}
+            pos="relative"
+            minW="360px"
+          >
+            <Icon
+              w="20px"
+              color="google"
+              pos="absolute"
+              left="16px"
+              as={FaGoogle}
+            />
+            Google
+          </Button>
+
+          <Flex alignItems="center" alignSelf="stretch">
+            <Divider />
+
+            <Text mx="4" whiteSpace="nowrap" fontSize="sm">
+              continue with
+            </Text>
+
+            <Divider />
+          </Flex>
+          <EmailPasswordSignUpForm handleEmailSignUp={handleEmailSignUp} />
+        </Stack>
+      </Stack>
+    </Flex>
+  );
+}
