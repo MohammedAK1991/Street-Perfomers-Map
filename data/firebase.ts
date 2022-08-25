41bf9d52b0570074c5cecef47553c579c5738f65
mohammedAK1991 <mohmed.ak.17@gmail.com>
2022-08-25 21:11:06 +0200

    chore(firebase): install latest version of firebase SDK

--- data/firebase.ts
+++ data/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app';
+import 'firebase/auth'
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCQegAH5vT-oHBxP1rcY7fPSes4U1X_CKc",
   authDomain: "callypso-c1252.firebaseapp.com",
@@ -16,6 +12,8 @@ const firebaseConfig = {
   measurementId: "G-XLP3195JF1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase
