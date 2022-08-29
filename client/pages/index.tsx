02e5f23c3a8b19273850e31e0c1ee5dd0954596e
mohammedAK1991 <mohmed.ak.17@gmail.com>
2022-08-29 12:52:03 +0200

    feat(index.tsx): update main UI to include a component to add, delete and update emails from the mailing list + UI to enter the body of the email to be bulk sent to the whole mailing list

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import { Heading, Box } from '@chakra-ui/react';
+import {
+  Flex,
+  Stack,
+  Box,
+  Heading,
+  Textarea,
+  InputGroup,
+  InputRightElement,
+  Input,
+  Button,
+  Divider,
+} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import firebase from '../data/firebase';
 import Header from '../components/Header';
+import useEmailAddresses, { addEmailAddress } from '../data/emailAddresses';
+import useAuth from '../data/useAuth';
+import EmailListItem from '../components/EmailListItem';
 
 export default function Home() {
   const router = useRouter();
@@ -15,8 +29,20 @@ export default function Home() {
     });
   }, [router]);
 
+  const { auth } = useAuth();
+
+  const [emailText, setEmailText] = useState('');
+  const [newEmailAddress, setNewEmailAddress] = useState('');
+
+  function handleInputChange(e) {
+    const inputValue = e.target.value;
+    setEmailText(inputValue);
+  }
+
+  const { emailAddresses, mutate } = useEmailAddresses();
+
   return (
-    <Box h="100vh" w="100vw" bg="white" display="flex">
+    <Box h="100vh" w="100vw" bg="white" display="flex" flexDirection="column">
       <Head>
         <title>Calllypso coding assignemt</title>
         <meta name="description" content="Calllypso coding assignemt" />
@@ -25,9 +51,65 @@ export default function Home() {
 
       <Header />
 
-      <Heading mx="auto" my="auto">
-        Welcome to callypso coding assignment
-      </Heading>
+      <Flex flexGrow={1}>
+        <Stack mr={10} p={['2', '8']}>
+          <Heading fontSize="2xl">Contacts</Heading>
+
+          <Stack pt={4} spacing={4}>
+            <InputGroup minWidth="350px">
+              <Input
+                value={newEmailAddress}
+                onChange={(e) => {
+                  setNewEmailAddress(e.target.value);
+                }}
+                placeholder="foo@bar.com"
+                w="full"
+              />
+              <InputRightElement>
+                <Button
+                  px="8"
+                  size="md"
+                  onClick={async () => {
+                    await addEmailAddress(auth, newEmailAddress);
+                    mutate();
+                    setNewEmailAddress('');
+                  }}
+                >
+                  Add
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            {emailAddresses
+              ? emailAddresses.map((emailListItem) => {
+                  return (
+                    <EmailListItem
+                      key={emailListItem.id}
+                      emailListItem={emailListItem}
+                    />
+                  );
+                })
+              : null}
+          </Stack>
+        </Stack>
+
+        <Divider height="100%" orientation="vertical" />
+
+        <Box flexGrow={1} p={['2', '8']}>
+          <Heading fontSize="2xl" mb="6">
+            Enter the body of the email below :
+          </Heading>
+          <Textarea
+            w="full"
+            p="4"
+            lineHeight={6}
+            minH="60vh"
+            value={emailText}
+            onChange={handleInputChange}
+            placeholder="Please enter the email body here. The email will be sent to all the emails from your mailing list on the left. You may edit, add or delete items from your mailing list as you wish"
+            size="sm"
+          />
+        </Box>
+      </Flex>
     </Box>
   );
 }
