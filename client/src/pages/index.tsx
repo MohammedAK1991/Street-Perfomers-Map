ea3d6e7158e43761af59b2ad28570e30ec162d69
MohammedAK1991 <ahmd199106@gmail.com>
2022-11-21 21:41:00 +0000

    extract the index page TABS into its own component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,22 +1,10 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
-import {
-  Heading,
-  Box,
-  Tabs,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Flex,
-  Center,
-  Text,
-  Button,
-  Stack,
-} from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import firebase from '../data/firebase';
-import Header from '../components/Header';
+import Header from '../components/common/Header';
+import LandingPageTabs from '../components/landingPage/tabs/LandingPageTabs';
 
 export default function Home() {
   const router = useRouter();
@@ -39,59 +27,14 @@ export default function Home() {
     >
       <Head>
         <title>Street Perfomers Map </title>
-        <meta name='description' content='Calllypso coding assignemt' />
+        <meta name='description' content='Street Perfomers Map' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
 
       <Flex mx={['50px', '200px']} my='200px'>
-        <Tabs
-          isFitted
-          variant='line'
-          height='500px'
-          width='500px'
-          bg='white'
-          borderRadius='20px'
-          p='3'
-        >
-          <TabList mb='1em' height='100px'>
-            <Tab>
-              <Heading size='md'>Artists</Heading>
-            </Tab>
-            <Tab>
-              <Heading size='md'>Music Lovers</Heading>
-            </Tab>
-            <Tab>
-              <Heading size='md'>Events</Heading>
-            </Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Flex flexGrow={1} direction='column'>
-                <Stack spacing='30px'>
-                  <Heading>
-                    Post your Peformance on the App and Make Money!
-                  </Heading>
-                  <Text>Yes without a doubt you will be making Money!</Text>
-                  <Flex>
-                    <Button
-                      variant='primary'
-                      size='lg'
-                      color='white'
-                      bg='black'
-                    >
-                      Make Money
-                    </Button>
-                  </Flex>
-                </Stack>
-              </Flex>
-            </TabPanel>
-            <TabPanel>
-              <p>two!</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+        <LandingPageTabs />
       </Flex>
     </Box>
   );
