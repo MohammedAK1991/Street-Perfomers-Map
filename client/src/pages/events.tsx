13f731bc6db1c9afd4bb591837ba0dd9e5206532
MohammedAK1991 <ahmd199106@gmail.com>
2022-11-21 21:40:19 +0000

    create index pages for events and music lovers

---
+++ client/src/pages/events.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type EventsPageProps = {};
+
+const EventsPage: React.FC<EventsPageProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default EventsPage;
